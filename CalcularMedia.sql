e52c10bb8a091df661ebb2e42af62bcdbac6b67d
DeveloperArthur <42553658+DeveloperArthur@users.noreply.github.com>
2019-03-01 18:04:06 -0300

    Update CalcularMedia.sql

--- CalcularMedia.sql
+++ CalcularMedia.sql
@@ -1,7 +1,10 @@
 DECLARE
-    nota1 number(4,2);
-    nota2 number(4,2);
-    media number(4,2); 
+    nota1 NUMBER(4,2);
+    nota2 NUMBER(4,2);
+    media NUMBER(4,2); 
 BEGIN
-    dbms_output.put_line('a media eh');
+    nota1:= &nota1;
+    nota2:= &nota2;
+    media:= (nota1+nota2)/2;
+    dbms_output.put_line('a media eh' || media);
 END;
