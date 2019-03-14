99e2e46d634c58397f66886a638c070cee41b15d
DeveloperArthur <42553658+DeveloperArthur@users.noreply.github.com>
2019-03-14 20:46:14 -0300

    Update HelloWorld.sql

--- HelloWorld.sql
+++ HelloWorld.sql
@@ -6,10 +6,3 @@ BEGIN
     text:= 'Hello World';
     dbms_output.put_line (text);
 END;
-
-
-CREATE TABLE ALUNO (
-RA NUMBER(5),
-NOME VARCHAR2(40));
-INSERT INTO ALUNO VALUES (1,'ANTONIO');
-INSERT INTO ALUNO VALUES (2,'BEATRIZ');
