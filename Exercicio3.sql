c642cffe6fde11eb42e19c9a4fef908a803920b5
DeveloperArthur <42553658+DeveloperArthur@users.noreply.github.com>
2019-03-21 20:45:26 -0300

    Add files via upload

---
+++ Exercicio3.sql
@@ -0,0 +1,9 @@
+DECLARE
+    v_numero FORNECEDOR.NUMERO%type := &NUMERO_DO_FORNECEDOR;
+BEGIN
+
+    delete from fornecedor where NUMERO = v_numero;
+    dbms_output.put_line('Fornecedor do numero: '||v_numero||
+                         ' foi excluido com sucesso');
+END;
+
