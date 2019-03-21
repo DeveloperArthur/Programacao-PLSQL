f4f0b275fcbd01996c82affa9eb0986fd1160934
DeveloperArthur <42553658+DeveloperArthur@users.noreply.github.com>
2019-03-21 20:35:24 -0300

    Add files via upload

---
+++ Exercicio2.sql
@@ -0,0 +1,17 @@
+desc fornecedor;
+select * from fornecedor;
+DECLARE
+    v_num FORNECEDOR.NUMERO%type := &NUMERO_DO_FORNECEDOR;
+    v_nome FORNECEDOR.NOME%type := '&NOME_DO_FORNECEDOR'; 
+    v_end FORNECEDOR.ENDERECO%type := '&ENDERECO_DO_FORNECEDOR';
+    v_cpf FORNECEDOR.CPF%type := &CPF_DO_CPF;
+    v_star FORNECEDOR.ESTRELAS%type := '&ESTRELAS_DO_FORNECEDOR';
+    v_vendas FORNECEDOR.VALORVENDAS%type := &VENDAS_DO_FORNECEDOR;
+    v_comissao FORNECEDOR.VALORCOMISSAO%type := &COMISSOES_DO_FORNECEDOR;
+BEGIN
+    insert into fornecedor values (v_num, v_nome, 
+                                   v_end, v_cpf, 
+                                   v_star, v_vendas, 
+                                   v_comissao, null, null);
+    
+END;
