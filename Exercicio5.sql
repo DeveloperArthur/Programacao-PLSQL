99669b5300ebb43496bd1eaa4ab9efb4d3110e5c
DeveloperArthur <42553658+DeveloperArthur@users.noreply.github.com>
2019-03-21 22:23:03 -0300

    Add files via upload

---
+++ Exercicio5.sql
@@ -0,0 +1,14 @@
+DECLARE
+    v_numeroQuery FORNECEDOR.NUMERO%type := &Digite_numero_a_ser_consultado;
+    v_numero FORNECEDOR.NUMERO%type;
+    v_nome FORNECEDOR.NOME%type;
+    v_end FORNECEDOR.ENDERECO%type;
+    v_vendas FORNECEDOR.VALORVENDAS%type;
+BEGIN
+    select numero, nome, endereco, Valorvendas
+    into v_numero, v_nome, v_end, v_vendas
+    from FORNECEDOR where NUMERO = v_numeroQuery;
+    
+    dbms_output.put_line('Busca realizada com sucesso');
+    dbms_output.put_line('Nome: '||v_nome||', Numero: '||v_numero||', Valor vendas: '||v_vendas||', Endereco: '||v_end);
+END;
