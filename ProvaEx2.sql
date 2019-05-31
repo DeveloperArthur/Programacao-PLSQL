699a091bdfbf71d17f694219db8258eb2390569d
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-05-30 21:00:56 -0300

    Update ProvaEx2.sql

--- ProvaEx2.sql
+++ ProvaEx2.sql
@@ -1,3 +1,26 @@
+/*2. Dado o bloco PL/SQL abaixo transformá-lo em função.
+DECLARE
+  V_soma NUMBER(3) := 0;
+  V_ini number(2) := &ini;
+  V_fim number(2) := &fim;
+  V_msg varchar2(100);
+BEGIN
+  FOR V_CONTADOR IN v_ini..v_fim
+  LOOP
+    V_soma := V_soma + V_CONTADOR;
+  END LOOP;
+  IF V_soma > 51 THEN
+    V_msg:='maior que 51';
+  END if;
+  IF V_soma < 51 THEN
+   V_msg:='menor que 51';
+  END IF;
+  IF V_soma = 51 THEN
+    V_msg:='igual a 51';
+  END IF;
+  DBMS_OUTPUT.PUT_LINE (V_msg);
+END;*/
+
 create or replace function ft_test
 (V_ini number, V_fim number)
 return varchar2
